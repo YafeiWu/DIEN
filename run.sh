3bbac581b431c769e10833c617e18c4a86be9d86
yafeiwu <yafeiw@opera.com>
2019-03-19 11:48:09 +0800

    rename dir

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ config=$1
 outdir=$2
 rm -rf ${outdir}
 mkdir ${outdir}
-mkdir ${outdir}/dnn_best_model
-mkdir ${outdir}/dnn_logdir ${outdir}/dnn_logdir/train ${outdir}/dnn_logdir/test
+mkdir ${outdir}/best_model
+mkdir ${outdir}/logdir ${outdir}/logdir/train ${outdir}/logdir/test
 CUDA_VISIBLE_DEVICES=0  /usr/bin/python2.7  script/train_dien.py train ${config} ${outdir} > ${outdir}/train_dien.log 2>&1 &
 echo "Training started!!"
