24292ded5b29aac077f2c20a574a5227f889cfc4
yafeiwu <yafeiw@opera.com>
2019-05-07 15:41:13 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-model=$1
-config=$2
-outdir=$3
+config=$1
+outdir=$2
 rm -rf ${outdir}
 mkdir ${outdir}
 mkdir ${outdir}/best_model
 mkdir ${outdir}/logdir ${outdir}/logdir/train ${outdir}/logdir/test
-CUDA_VISIBLE_DEVICES=0  /usr/bin/python2.7  script/train_sess.py train ${config} ${outdir} > ${outdir}/train_sess.log 2>&1 &
+CUDA_VISIBLE_DEVICES=0 /usr/bin/python2.7 script/train_sess.py train ${config} ${outdir} > ${outdir}/train_sess.log 2>&1 &
 echo "Training started!!"
